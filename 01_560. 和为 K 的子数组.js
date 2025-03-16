c7798e1d944ed9a8f40cc20a7656d9c65bac9e82
steve xiaohu zhao <steve_zhao@163.com>
2025-03-16 10:43:03 +0800

    code partly

--- 01_560. 和为 K 的子数组.js
+++ 01_560. 和为 K 的子数组.js
@@ -10,8 +10,10 @@ let subarraySum = function (nums, k) {
     // 初始化哈希表中的第一个元素: 前缀和为 0 的情况默认出现 1 次
     prevSumMap.set(0, 1);
 
-    // 2. 定义用来记录当前前缀和的变量 sum 和用来统计当前满足他条件的字数组的个数变量 count
-    let sum = 0, count = 0;
+    // 2. 定义用来记录当前前缀和的变量 sum
+    let sum = 0;
+    // count 表示: 和为 k 的子数组的总数
+    let count = 0;
 
     // 3. 遍历数组，统计截止每个元素的前缀和
     // [1, 2, 3, 4, 5, 6]
@@ -19,8 +21,17 @@ let subarraySum = function (nums, k) {
         // 统计以每个元素结尾的，前缀和
         sum += nums[i];
 
+        // prevSumMap.get(sum - k) 表示以当前遍历位置 i 结尾的、和为 k 的子数组数量
+        if (prevSumMap.has(sum - k)) {
+            count += prevSumMap.get(sum - k);
+        }
+
         // 将每个前缀和都加入哈希表中
-        prevSumMap.set(sum, 1);
+        prevSumMap.set(sum, (prevSumMap.get(sum) || 0) + 1);
     }
 
+    return count;
+
 }
+
+subarraySum([1, 2, 3, 4, 5, 6], 2);
